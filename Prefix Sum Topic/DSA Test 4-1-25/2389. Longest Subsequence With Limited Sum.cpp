82166baa2eed52f9fe1eab37274ad65936cb1181
SHIVAM SHRIVASTAV <shivamshrivastav26112005@gmail.com>
2025-01-06 23:37:48 +0530

    Longest Subsequence With Limited Sum Ques. Added.

---
+++ Prefix Sum Topic/DSA Test 4-1-25/2389. Longest Subsequence With Limited Sum.cpp
@@ -0,0 +1,65 @@
+Q.2389-: Longest Subsequence With Limited Sum.
+
+You are given an integer array nums of length n, and an integer array queries of length m.
+Return an array answer of length m where answer[i] is the maximum size of a subsequence that 
+you can take from nums such that the sum of its elements is less than or equal to queries[i].
+A subsequence is an array that can be derived from another array by deleting some or no elements
+without changing the order of the remaining elements.
+
+ 
+
+Example 1:
+Input: nums = [4,5,2,1], queries = [3,10,21]
+Output: [2,3,4]
+Explanation: We answer the queries as follows:
+- The subsequence [2,1] has a sum less than or equal to 3. It can be proven that 2 is the maximum size of such a subsequence, so answer[0] = 2.
+- The subsequence [4,5,1] has a sum less than or equal to 10. It can be proven that 3 is the maximum size of such a subsequence, so answer[1] = 3.
+- The subsequence [4,5,2,1] has a sum less than or equal to 21. It can be proven that 4 is the maximum size of such a subsequence, so answer[2] = 4.
+
+Example 2:
+Input: nums = [2,3,4,5], queries = [1]
+Output: [0]
+Explanation: The empty subsequence is the only subsequence that has a sum less than or equal to 1, so answer[0] = 0.
+ 
+
+Constraints:
+n == nums.length
+m == queries.length
+1 <= n, m <= 1000
+1 <= nums[i], queries[i] <= 106  
+
+
+
+Code In C++ -: 
+
+
+class Solution { 
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        int n=nums.size(); 
+        int m=queries.size(); 
+
+        vector<int>ans(m);
+        sort(nums.begin(), nums.end());
+
+        vector<int>pre(n); 
+        pre[0]=nums[0];
+        for(int i=1; i<n; i++){
+            pre[i]=nums[i]+pre[i-1]; 
+        }
+
+        for(int i=0; i<m; i++){
+            int count=0; 
+            for(int j=0; j<n; j++){ 
+                if(pre[j]<=queries[i]){
+                    count=j+1;
+                }else{
+                    break;
+                }
+                ans[i]=count; 
+            }
+        }
+        return ans; 
+    }
+};
+
